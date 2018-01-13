b335644531ccc34e64165e802b8a2c0bb7d4ab17
DESKTOP-K50CTHE\ilyon <il.yonimor@gmail.com>
2018-01-13 12:16:40 -0800

    style main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import { Button } from 'react-bootstrap';
+import '../styles/Main.scss';
+import Logo from '../assets/images/logo.png';
 
 @observer
 class Main extends Component {
@@ -17,11 +20,14 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-        <h1>Main</h1>
-        <button
-          className="logout"
-          onClick={this.onLogout}
-        >Logout</button>
+        <header className="main-header">
+          <img className="main-logo" src={Logo} alt="Logo"/>
+          <Button
+            className="logout"
+            onClick={this.onLogout}
+            bsStyle="primary"
+          >Logout</Button>
+        </header>
       </div>
     );
   }
